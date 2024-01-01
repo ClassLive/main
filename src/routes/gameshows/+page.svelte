f0216c68d331dedc5cd5b21e98ac98acc6524744
nmsderp <130254323+nmsderp@users.noreply.github.com>
2023-12-31 19:43:00 -0500

    start on gameshows page

---
+++ src/routes/gameshows/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Gameshows</title>
+	<meta name="description" content="Gameshows" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>GamesShows:</h1>
+
+	<p>
+		We currently do not have any finished gameshows, check back soon!
+	</p>
+</div>
