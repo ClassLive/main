9587f84f5134afc2c16e404b49bc8382129fbfdf
nmsderp <nmsderp@gmail.com>
2023-12-31 19:19:03 -0500

    first commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About ClassLive</h1>
+
+	<p>
+		ClassLive is a free and opensource live classroom gameshow, you will never have to pay a single penny on this.
+	</p>
+
+
+	<p>
+		We currently have 1 game! But there will be many more in the future!
+	</p>
+</div>
